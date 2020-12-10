6c5d65f23f80f5ef17aa47c67eec8716f98c9e67
Jangdin <hampusjangdin@gmail.com>
2020-12-10 21:09:46 +0100

    FocusVyn är uppbyggd
    Alla saker för UI finns på sidan, är dock inte placerade på rätt ställe. Detta kan någon sitta med imorn

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:projectapp/RecipeView.dart';
+import 'package:projectapp/FocusRecipeView.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: RecipeView(),
+      home: FocusRecipeView(),
     );
   }
 }
